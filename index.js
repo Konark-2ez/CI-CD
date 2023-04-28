b47767f9a50114ddc3672c37f4c3892da6b7c3f1
Konark-2ez <konarkkumar456@gmail.com>
2023-04-28 12:47:12 +0530

    Github action class code

---
+++ index.js
@@ -0,0 +1,11 @@
+const str = 'quote'
+
+console.log(str)
+const str1 = 'for you'
+console.log(str + str1)
+
+if (str.length > 1) {
+    console.log('trueeeee')
+} else {
+    console.log('Nooooo')
+}
